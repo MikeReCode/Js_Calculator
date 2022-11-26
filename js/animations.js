2f66e2ed5df5e4ed5a3f1172fed71810c2bf72c8
MikeReCode <mihail.radu.work@gmail.com>
2022-11-26 20:41:52 +0200

    Add animation to buttons

--- js/animations.js
+++ js/animations.js
@@ -2,18 +2,27 @@ const all = document.querySelectorAll(".btn")
 
 all.forEach(element => {
     element.addEventListener("click", ()=>{
-        const cliked = document.querySelector(`[id="${element.id}"]`)
-        cliked.classList.add("clicado");
+        const clicked = document.querySelector(`[id="${element.id}"]`)
+        
+        let classes = clicked.className.split(" ")
+        if (classes.includes("number") || element.id === "dot" || element.id === "delete" || element.id === "clear"){
+            clicked.classList.add("clicado");
+        }else if(element.id === "equal"){
+            clicked.classList.add("clicado-equal");
+        }else{
+            clicked.classList.add("clicado-action");
+        }
+        
     })
+    
+    element.addEventListener("transitionend", removeclass)
     
 });
 
-all.forEach(element => {
-    
-    element.addEventListener("transitionend", removeclass)
-});
 
 function removeclass (e) {
     if (e.propertyName !== "transform") return
         this.classList.remove("clicado");
+        this.classList.remove("clicado-action");
+        this.classList.remove("clicado-equal");
 }
